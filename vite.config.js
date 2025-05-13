47a21cecdd23dc0659b7331633f76fa2e9261e04
Joe Conway <joeconway@Joes-MacBook-Pro.local>
2025-05-13 12:19:39 +0100

    ✅ Fix plugin import for yaml support

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import stringPlugin from 'vite-plugin-string';
+import stringPlugin from 'vite-plugin-string'; // <— THIS IS CORRECT
 
 export default defineConfig(({ command, mode }) => {
   const isWorkerBuild = process.env.BUILD_TARGET === 'worker';
