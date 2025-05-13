9c9a8bdca6a0b3724ae4c70cf15b917cf62cea58
Joe Conway <joeconway@Joes-MacBook-Pro.local>
2025-05-13 11:26:50 +0100

    ✅ Fix duplicate painusProfile declaration

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,27 @@
-import { defineConfig } from 'vite';
-import stringPlugin from 'vite-plugin-string';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import stringPlugin from "vite-plugin-string";
 
 export default defineConfig(({ command, mode }) => {
-  const isWorkerBuild = process.env.BUILD_TARGET === 'worker';
+  const isWorkerBuild = process.env.BUILD_TARGET === "worker";
 
   return {
     plugins: [
-      isWorkerBuild && stringPlugin({
-        include: ['**/*.yaml'], // tell it what files to load
-      })
+      !isWorkerBuild && react(),
+      isWorkerBuild && stringPlugin()
     ].filter(Boolean),
     build: isWorkerBuild
       ? {
-          target: 'esnext',
-          outDir: 'dist',
+          target: "esnext",
+          outDir: "dist",
           lib: {
-            entry: 'src/worker.js',
-            formats: ['es'],
-            fileName: () => 'worker.js'
+            entry: "src/worker.js",
+            formats: ["es"],
+            fileName: () => "worker.js"
           },
+          rollupOptions: {
+            external: [],
+          }
         }
       : {}
   };
